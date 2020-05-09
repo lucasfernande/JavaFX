c8564bdd6d3753394a319b99d9fa0147aa8601df
lucasfernande <lucas.fernandes6192@outlook.com>
2020-05-09 14:50:44 -0300

    ComboBox

---
+++ ComboBox/src/gui/util/Constraints.java
@@ -0,0 +1,29 @@
+package gui.util;
+
+import javafx.scene.control.TextField;
+
+public class Constraints {
+	public static void setTextFieldInteger(TextField txt) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (newValue != null && !newValue.matches("\\d*")) { // testando se o número existe e se é inteiro
+				txt.setText(oldValue); // caso não seja, o valor não será aceito
+			}
+		});
+	}
+
+	public static void setTextFieldMaxLength(TextField txt, int max) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (newValue != null && newValue.length() > max) {
+				txt.setText(oldValue);
+			}
+		});
+	}
+
+	public static void setTextFieldDouble(TextField txt) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (newValue != null && !newValue.matches("\\d*([\\.]\\d*)?")) {
+				txt.setText(oldValue);
+			}
+		});
+	}
+}
